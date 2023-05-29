eb4086395c8fb10c147c583d5859ced49f10fea3
Gahoo <rmit25@mail.ru>
2023-05-30 00:19:15 +0600

    Main file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyGraph<Integer> graph = new MyGraph<>();
+
+        // Create vertices
+        graph.addVertex(0);
+        graph.addVertex(1);
+        graph.addVertex(2);
+
+        graph.addEdge(0,1, 10);
+        graph.addEdge(0,2,1);
+        graph.addEdge(2,1,2);
+
+        graph.Dijkstra(0);
     }
 }
